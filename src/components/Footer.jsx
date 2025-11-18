fe360d28a6dee899594a7bae3bf4fa531570647a
ri-uso <ri-uso@centroqualitatessile.it>
2025-11-18 11:12:41 +0100

    Update footer image source to Centergross footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer(){
   return (
     <footer className="container custom-footer">
       <img
-        src="/images/Footer-DPP-browser.png"
+        src="/images/FooterCentergross.png"
         alt="Progetto realizzato grazie ai fondi europei della Regione Emilia-Romagna"
         className="footer-image"
         loading="lazy"
